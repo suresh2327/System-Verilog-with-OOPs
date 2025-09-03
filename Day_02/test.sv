62f83b40d344eacad736a81ebd9939d007ec1562
suresh2327 <sureshgadi68@gmail.com>
2025-09-03 21:01:00 +0530

    fork join

---
+++ Day_02/test.sv
@@ -0,0 +1,24 @@
+module tb;
+  task one();
+    $display("task1 is started = %0t",$time);
+    #10;
+    $display("task1 is completed = %0t",$time);
+  endtask
+  task two();
+    $display("task2 is started = %0t",$time);
+     #10;
+    $display("task2 is completed = %0t",$time);
+  endtask
+  task three();
+    $display("task3 is started = %0t",$time);
+     #10;
+    $display("task3 is completed = %0t",$time);
+  endtask
+  initial begin
+    fork
+      one();
+      two();
+    join
+    three();
+  end
+endmodule
