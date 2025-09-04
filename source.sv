f2a6cf6c6f39e6a1859f54427aa3d2eb12106827
suresh2327 <sureshgadi68@gmail.com>
2025-09-04 09:55:30 +0530

    source

--- source.sv
+++ source.sv
@@ -1177,6 +1177,33 @@ endmodule
 //outputs
 # KERNEL: Data=29 Data1=17,Data2=555
 
+//class in class code 
+// Code your testbench here
+// or browse Examples
+class first;
+  int data=50;
+endclass
+
+class second;
+  first f;
+  function new();
+    f=new();
+  endfunction
+endclass
+module tb;
+  second s;
+  initial begin
+    s=new();
+    $display("data=%0d",s.f.data);
+    s.f.data=45;
+        $display("data=%0d",s.f.data);
+  end
+endmodule
+
+//output:
+# KERNEL: data=50
+# KERNEL: data=45
+
 
 
 
