9efd19394db40ac982ddcac277709539c3a4168c
suresh2327 <sureshgadi68@gmail.com>
2025-09-06 10:33:32 +0530

    source

--- source.sv
+++ source.sv
@@ -1376,9 +1376,73 @@ Deep Copy:
 --> Copy Field by field from source to destination.
 */
 
+class packet;
+  int count;
+  function void display(string prefix="");
+    $display("%s packet: count=%0d", prefix, count);
+  endfunction
+endclass
 
 
+class sample;
+  int a;       // Non-object variable
+  int b;       // Non-object variable
+  packet pck;  // Object variable
 
+  function new();
+    pck = new();  
+  endfunction
+
+  // Deep copy
+  function void deep_copy(sample src);
+    this.a = src.a;
+    this.b = src.b;
+    this.pck = new();               
+    this.pck.count = src.pck.count;  
+  endfunction
+
+ 
+  function void display(string prefix="");
+    $display("%s sample: a=%0d b=%0d", prefix, a, b);
+    pck.display({prefix,"  "});
+  endfunction
+endclass
+
+
+module tb;
+  sample s1, s2;
+
+  initial begin
+    s1 = new();
+    s2 = new();
+
+    s2.a = 100;
+    s2.pck.count = 50;
+
+    s1.deep_copy(s2);   
+    s1.display("s1");
+    s2.display("s2");
+
+    $display("---- Modify s2 ----");
+    s2.a = 200;
+    s2.b = 300;
+    s2.pck.count = 600;
+
+    s1.display("s1");
+    s2.display("s2");
+  end
+endmodule
+
+//output
+# KERNEL: s1 sample: a=100 b=0
+# KERNEL: s1   packet: count=50
+# KERNEL: s2 sample: a=100 b=0
+# KERNEL: s2   packet: count=50
+# KERNEL: ---- Modify s2 ----
+# KERNEL: s1 sample: a=100 b=0
+# KERNEL: s1   packet: count=50
+# KERNEL: s2 sample: a=200 b=300
+# KERNEL: s2   packet: count=600
 
 
 
